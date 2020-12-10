308ccf9a1e1fe476345a3048d42bfdbdd0262268
Raymond Lo <raymond.lo@yahoo.com>
2020-12-10 01:37:58 -0500

    included footer section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import "fontsource-merriweather";
 import "normalize.css";
 import Landing from '../sections/Landing/Landing';
 import Project from '../sections/Projects/Project';
+import Footer from '../sections/Footer/Footer';
 
 export default function Home() {
   return (
     <main>
       <Landing />
       <Project />
+      <Footer />
     </main>
   )
 }
