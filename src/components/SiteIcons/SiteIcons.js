758dc3ef5fac91adbda72a61e963b3886a15c1eb
Raymond Lo <raymond.lo@yahoo.com>
2020-12-10 01:37:05 -0500

    added config props to style component

--- src/components/SiteIcons/SiteIcons.js
+++ src/components/SiteIcons/SiteIcons.js
@@ -5,21 +5,21 @@ import FileIcon from '../../../static/icons/file-icon.svg';
 import LinkedInIcon from '../../../static/icons/LinkedIn-icon.svg';
 
 
-function SiteIcons() {
+function SiteIcons({lineColor, marginTop, iconColor}) {
     const {line, icon, iconsContainer, siteIconsContainer} = siteIconStyles;
 
     return (
-        <div className={siteIconsContainer}>
-            <hr className={line}></hr>
+        <div style={{marginTop: marginTop}} className={siteIconsContainer}>
+            <hr style={{color: lineColor }} className={line}></hr>
             <div className={iconsContainer}>
                 <a href="https://github.com/rayy-lo">
-                    <img className={icon} src={GitHubIcon} alt="GitHub icon" />
+                    <img style={{filter: iconColor}} className={icon} src={GitHubIcon} alt="GitHub icon" />
                 </a>
                 <a href="https://www.linkedin.com/in/raymond-lo-323b1b178/">       
-                    <img className={icon} src={LinkedInIcon} alt="LinkedIn icon" />
+                    <img style={{filter: iconColor}} className={icon} src={LinkedInIcon} alt="LinkedIn icon" />
                 </a>
                 <a href="" download>
-                    <img className={icon} src={FileIcon} alt="Resume file icon" />
+                    <img style={{filter: iconColor}} className={icon} src={FileIcon} alt="Resume file icon" />
                 </a>
             </div>
         </div>
