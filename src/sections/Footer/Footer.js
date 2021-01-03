a0d0f9e610abc81452dc69919e8506c15d7b45f6
Raymond Lo <raymond.lo@yahoo.com>
2021-01-03 02:14:50 -0500

    replaced img tags for div tags with bg-img css

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
 import styles from './footer.module.css';
 import SiteIcons from '../../components/SiteIcons/SiteIcons';
 import OpenContactBtn from '../../components/OpenContactBtn/OpenContactBtn';
-import SquaresSVG from '../../../static/icons/pinkSquares.svg';
 
-function Footer({toggleModal}) {
-    const {footer, squareIcon, textWrapper, header, copyright, subheader, headerWrapper} = styles;
+function Footer({ toggleModal }) {
+    const {
+        footer,
+        squareIcon,
+        textWrapper,
+        header,
+        copyright,
+        subheader,
+        headerWrapper,
+    } = styles;
     const date = new Date();
     const year = date.getFullYear();
 
     //filter css color of svg icon
-    const cssFilterValueForWhite = 'invert(100%) sepia(100%) saturate(0%) hue-rotate(267deg) brightness(103%) contrast(104%)';
+    const cssFilterValueForWhite =
+        'invert(100%) sepia(100%) saturate(0%) hue-rotate(267deg) brightness(103%) contrast(104%)';
 
     const openModalBtnStyles = {
         height: '50px',
@@ -18,29 +26,82 @@ function Footer({toggleModal}) {
         color: 'white',
         background: '#D93B3B',
         border: '1px solid #E14B4B',
-        borderRadius: '10px'
-    }
+        borderRadius: '10px',
+    };
 
     return (
         <footer className={footer}>
             <div className={textWrapper}>
                 <div className={headerWrapper}>
-                    <h1 className={header}>Interested in <br />working together?</h1>
-                    <span className={subheader}>Send me an email and let's get started!</span>
+                    <h1 className={header}>
+                        Interested in <br />
+                        working together?
+                    </h1>
+                    <span className={subheader}>
+                        Send me an email and let's get started!
+                    </span>
                 </div>
-                <OpenContactBtn toggleModal={toggleModal} openModalBtnStyles={openModalBtnStyles} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(-700px, 0) rotate(90deg)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(-800px, -100px) rotate(180deg)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(0px, 0) rotate(45deg)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(200px, -100px)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(450px, -160px) rotate(300deg)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(650px, -100px)'}} className={squareIcon} />
-                <img src={SquaresSVG} alt="" style={{transform: 'translate(800px, -150px) rotate(180deg)'}} className={squareIcon} />
+                <OpenContactBtn
+                    toggleModal={toggleModal}
+                    openModalBtnStyles={openModalBtnStyles}
+                />
+                <div
+                    style={{ transform: 'translate(-700px, 0) rotate(90deg)' }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{
+                        transform: 'translate(-800px, -100px) rotate(180deg)',
+                    }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{ transform: 'translate(0px, 0) rotate(45deg)' }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{ transform: 'translate(200px, -100px)' }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{
+                        transform: 'translate(450px, -160px) rotate(300deg)',
+                    }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{ transform: 'translate(650px, -100px)' }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
+                <div
+                    style={{
+                        transform: 'translate(800px, -150px) rotate(180deg)',
+                    }}
+                    className={squareIcon}
+                    role="img"
+                    alt=""
+                />
             </div>
-            <SiteIcons lineColor='white' marginTop='1em' iconColor={cssFilterValueForWhite} />
+            <SiteIcons
+                lineColor="white"
+                marginTop="1em"
+                iconColor={cssFilterValueForWhite}
+            />
             <span className={copyright}> &#169; {year} Raymond Lo </span>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
