8f295bf5613de56842f2583d74ce2656760317bb
Raymond Lo <raymond.lo@yahoo.com>
2021-01-03 02:12:44 -0500

    updated p3 description

--- static/projectData.js
+++ static/projectData.js
@@ -1,4 +1,4 @@
-import CalendappImage from '../static/projectImages/Calendapp.svg'
+import CalendappImage from '../static/projectImages/Calendapp.svg';
 import ShopifyIcon from '../static/projectImages/Shopify.svg';
 import HTML5Icon from '../static/icons/html.svg';
 import JavaScriptIcon from '../static/icons/javascript.svg';
@@ -13,26 +13,39 @@ import LiquidIcon from '../static/icons/liquid.svg';
 const data = [
     {
         id: 1,
-        tech: [{icon: LiquidIcon, name:"Liquid"}, {icon: JavaScriptIcon, name:"JavaScript"}, {icon: HTML5Icon, name:"HTML"}, {icon: CSSIcon, name: "CSS"}, {icon: jQueryIcon, name: "jQuery"}],
+        tech: [
+            { icon: LiquidIcon, name: 'Liquid' },
+            { icon: JavaScriptIcon, name: 'JavaScript' },
+            { icon: HTML5Icon, name: 'HTML' },
+            { icon: CSSIcon, name: 'CSS' },
+            { icon: jQueryIcon, name: 'jQuery' },
+        ],
         info: {
-            type: "Web Developer",
-            name: "P3 Media",
-            description: "Developed various features for Shopify merchants. An extremely popular request was a gift with purchase feature, which generated over $1.5 million for a store within a couple hours. Improved site speed, and fixed numerous JavaScript and style bugs.",
+            type: 'Web Developer',
+            name: 'P3 Media',
+            description:
+                'Developed various features for Shopify merchants. An extremely popular request was a gift with purchase feature, which generated over $1.5 million for a store within a couple hours. Improved site speed, and fixed numerous JavaScript and style bugs. Built a reusable, native web component carousel to display store promotions.',
         },
-        repo: "",
-        image: ShopifyIcon
+        repo: '',
+        image: ShopifyIcon,
     },
     {
-        info:{
-            type: "Scheduler App",
-            name: "Calendapp",
-            description: "Calendapp is a web application inspired by Calendly to allow users to schedule meetings more easily. Integrated with Google Calendar API to ensure there are no conflicts in schedule. Also includes a premium plan which allows a custom length of meeting. Default 60 minute length meetings for free.",
+        info: {
+            type: 'Scheduler App',
+            name: 'Calendapp',
+            description:
+                'Calendapp is a web application inspired by Calendly to allow users to schedule meetings more easily. Integrated with Google Calendar API to ensure there are no conflicts in schedule. Also includes a premium plan which allows a custom length of meeting. Default 60 minute length meetings for free.',
         },
-        repo: "https://github.com/hatchways/team-carbonara",
-        tech: [{icon: ReactIcon, name:"React"}, {icon: NodeJSIcon, name: "NodeJS"}, {icon:ExpressJSIcon, name: "Express"}, {icon: MongoDBIcon, name: "MongoDB"}],
+        repo: 'https://github.com/hatchways/team-carbonara',
+        tech: [
+            { icon: ReactIcon, name: 'React' },
+            { icon: NodeJSIcon, name: 'NodeJS' },
+            { icon: ExpressJSIcon, name: 'Express' },
+            { icon: MongoDBIcon, name: 'MongoDB' },
+        ],
         id: 2,
-        image: CalendappImage
+        image: CalendappImage,
     },
-]
+];
 
 export default data;
